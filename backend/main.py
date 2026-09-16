405b7a2671eac17720ba9b306d42449198d8cfa8
Samyak01AI <samyakpawar669@gmail.com>
2026-09-16 20:14:39 +0530

    Url changed again for backend 1.1

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="EcoGrid AI", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ecogrid-xy92-cbwvdwky5-ethhacker01s-projects.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
